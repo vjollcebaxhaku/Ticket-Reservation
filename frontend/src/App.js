cb6a41981c2c6359352a8198ce520b06ff284ea8
blerta xheladini <blerta.xheladini03@gmail.com>
2024-04-22 00:38:12 +0200

    small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Tweet from './components/Tweet';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Nav />
+          <Routes>
+            <Route path="/" exact component={Home} />
+            <Route path="/tweets" exact component={Tweet} />
+            </Routes>
+      </div>
+    </Router>
   );
 }
 
