476cfc28627a4c42147f691cd65e59fc371592d2
BlertaXheladini <blerta.xheladini03@gmail.com>
2024-05-23 09:38:23 +0200

    changes

--- frontend/src/AdminRoutes.js
+++ frontend/src/AdminRoutes.js
@@ -1,10 +1,7 @@
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 
 const AdminRoutes = () => {
-    const isAdmin = sessionStorage.getItem('currentUserRole') === 'admin';
-    return(
-        isAdmin ? <Outlet/> : <Navigate to="/not-found"/>
-    )
-}
+    return <Outlet />;
+};
 
-export default AdminRoutes
+export default AdminRoutes;
